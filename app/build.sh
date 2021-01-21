cc2f37366c6f494a5113b400b9cc1283597665e8
Taner Gokalp <taner.gokalp@shazam.com>
2021-01-21 11:34:29 +0100

    removed nodemodules removal

--- app/build.sh
+++ app/build.sh
@@ -2,12 +2,8 @@
 
 echo 'Building Nest'
 
-npm install --only=development
+npm install
 
 npm run build
-
-rm -rf node_modules
-
-npm install --only=production
 
 #docker build -t nest_app -f Dockerfile .
