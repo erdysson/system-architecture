2aca4ce339b06ac068a15d96368d2cb115e1e3ee
Taner Gokalp <taner.gokalp@shazam.com>
2021-01-23 15:06:42 +0100

    fixed nginx stop script

--- nginx/scripts/stop.sh
+++ nginx/scripts/stop.sh
@@ -2,6 +2,6 @@
 
 echo 'Starting Nginx Load balancer container...'
 
-docker stop nginx_lb_container${BUILD_NUMBER}
+docker stop nginx_lb_container_${BUILD_NUMBER}
 
 echo 'Stopped Nginx Load balancer container'
