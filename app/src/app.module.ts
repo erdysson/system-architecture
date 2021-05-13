8b7be7b8c3071a295760879f99990ce8952526d4
egoekalp <erditaner.goekalp@eqs.com>
2021-05-13 18:29:31 +0200

    added env files

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,4 +1,7 @@
+import * as path from 'path';
+
 import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
 import {MongooseModule} from '@nestjs/mongoose';
 
 import {AuthController} from './controllers/auth/auth.controller';
@@ -8,18 +11,21 @@ import {User, UserSchema} from './schemas/user.schema';
 import {AuthService} from './services/auth.service';
 import {UserService} from './services/user.service';
 
+export const nodeEnv = process.env.NODE_ENV || 'development';
+
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: path.join(process.cwd(), `env/.${nodeEnv}.env`)
+        }),
         MongooseModule.forFeature([
             {
                 name: User.name,
                 schema: UserSchema
             }
         ]),
-        // enable for docker compose
-        // MongooseModule.forRoot('mongodb://mongodb:27017/system-architecture')
-        // enable for local dev
-        MongooseModule.forRoot('mongodb://127.0.0.1:27017/system-architecture')
+        MongooseModule.forRoot(process.env.MONGODB_URL)
     ],
     controllers: [AuthController, UserController],
     providers: [AuthService, AuthGuard, UserService]
